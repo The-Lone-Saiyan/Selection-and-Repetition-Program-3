26f25153e00157fdd4baafbb485fa55ec349c28c
Roberto Fitzhugh <rgf274@email.vccs.edu>
2024-02-12 15:46:52 -0500

    In Progress

--- Selection and Repetition Program 3.cpp
+++ Selection and Repetition Program 3.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
+ifstream inFile1, inFile2;
+ofstream outFile;
+string line1, line2;
+
 int main()
 {
-
+	inFile1.open("File 1.txt");
+	inFile2.open("File 2.txt");
+	getline(inFile1, line1);
+	getline(inFile2, line2);
+	outFile.open("Merged File.txt");
 
 	return 0;
 }
