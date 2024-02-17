3aff205fe4503a0a6f467013dab508640739479c
Roberto Fitzhugh <thelonesaiyan.xboxlive@yahoo.com>
2024-02-17 15:36:13 -0500

    Final

--- Selection and Repetition Program 3.cpp
+++ Selection and Repetition Program 3.cpp
@@ -13,18 +13,41 @@ int main()
 {
 	inFile1.open("File 1.txt");
 	inFile2.open("File 2.txt");
-	
 	outFile.open("Merged File.txt");
+
+	getline(inFile1, Line1);
+	getline(inFile2, Line2);
 
 	while (!inFile1.eof() && !inFile2.eof())
 	{
-		getline(inFile1, Line1);
-		getline(inFile2, Line2);
-			
-		outFile << Line1 << endl;
-		outFile << Line2 << endl;	
+		if (Line1 < Line2)
+		{
+			outFile << Line1 << endl;
+			getline(inFile1, Line1);
+		}
+		else
+		{
+			outFile << Line2 << endl;
+			getline(inFile2, Line2);
+		}
 	}
 
-	inFile1.close("File 1.txt");
+	while (!inFile1.eof())
+	{
+		outFile << Line1 << endl;
+		getline(inFile1, Line1);
+	}
+
+	while (!inFile2.eof())
+	{
+		outFile << Line2 << endl;
+		getline(inFile2, Line2);
+	}
+
+
+	inFile1.close();
+	inFile2.close();
+	outFile.close();
+
 	return 0;
 }
